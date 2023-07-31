1da282fc982068b37de0c7fdc9a3df236e6e901c
Camilo <arqcamilocardenasmesa@gmail.com>
2023-07-31 17:40:29 -0500

    feat: add button text as prop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import Button from "./Button";
+
 function App() {
-    return <div>App</div>
+    return (
+        <div>
+            <div>
+                <Button>Click here!</Button>
+            </div>
+            <div>
+                <Button>By Now!</Button>
+            </div>
+            <div>
+                <Button>Hi</Button>
+            </div>
+            <div>
+                <Button>Someting</Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
