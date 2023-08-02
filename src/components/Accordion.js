6a759b5b798ded8ef6e9809a1cbe3784510b4da4
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-08-02 16:48:52 -0500

    style: box model styling

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,4 @@
-import { GoMoveToBottom, GoMoveToStart } from "react-icons/go";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 import { useState} from "react";
 
 function Accordion({ items }) {
@@ -11,22 +11,24 @@ function Accordion({ items }) {
     const renderItems = items.map((item, index) => {
         const isExpanded = index === expandedItems;
 
-        const icon = <span>
-            {isExpanded ? <GoMoveToBottom /> : <GoMoveToStart />}
+        const icon = (
+        <span className="text-2xl">
+            {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
         </span>
+        );
 
         return (
             <div key={item.id}>
-                <div onClick={() => handleClick(index)}>
-                    {icon}
+                <div className="flex justify-between p-3 bg-gray-100 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
                     {item.label}
+                    {icon}
                 </div>
-                {isExpanded && <div>{item.content}</div>}
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         );
     });
 
-    return <div>{renderItems}</div>
+    return <div className="border-x border-t rounded">{renderItems}</div>
 }
 
 export default Accordion;
