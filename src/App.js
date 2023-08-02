0d38702cb1e69923d6330eadb6aa647ae7acd887
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-08-01 19:43:20 -0500

    expanded and collapse behavior

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import { GoBell, GoCloud, GoDatabase } from 'react-icons/go';
-import Button from "./components/Button";
+import Accordion from "./components/Accordion";
 
 function App() {
-    return (
-        <div>
-            <div>
-                <Button primary outline className="mb-4">
-                    <GoBell />
-                    Click here
-                </Button>
-            </div>
-            <div>
-                <Button outline success>
-                    <GoCloud />
-                    By Now!
-                </Button>
-            </div>
-            <div>
-                <Button outline warning>
-                    <GoDatabase />
-                    See Deal
-                </Button>
-            </div>
-            <div>
-                <Button secondary outline>
-                    Hi there
-                </Button>
-            </div>
-            <div>
-                <Button danger rounded outline>
-                    Someting
-                </Button>
-            </div>
-        </div>
-    );
+    const items = [
+        {
+            id: 'asdvasrfv54',
+            label: 'Can I use React on a project?',
+            content: 'You can use everyting you want. You can use everyting you want. You can use everyting you want'
+        },
+        {
+            id: 'afnrgfl',
+            label: 'Can I use Javascript on a project?',
+            content: 'You can use everyting you want little bird. You can use everyting you want little bird. You can use everyting you want little bird'
+        },
+        {
+            id: 'dgbns',
+            label: 'Can I use Css on a project?',
+            content: 'You can use everyting you want my little cat. You can use everyting you want my little cat. You can use everyting you want my little cat'
+        },
+    ];
+
+   return <Accordion items={items} />
 }
 
 export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,20 @@
+import { useState} from "react";
+
+function Accordion({ items }) {
+    const [expandedItems, setExpandedItems] = useState(0);
+
+    const renderItems = items.map((item, index) => {
+        const isExpanded = index === expandedItems;
+
+        return (
+            <div key={item.id}>
+                <div>{item.label}</div>
+                {isExpanded && <div>{item.content}</div>}
+            </div>
+        );
+    });
+
+    return <div>{renderItems}</div>
+}
+
+export default Accordion;
