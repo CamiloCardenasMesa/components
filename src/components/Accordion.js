669a59b259f4550b9e6cec07dcb774119e7aa8fa
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-08-02 16:18:28 -0500

    refactor(event handler): defined outside of mapping function

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,12 +3,16 @@ import { useState} from "react";
 function Accordion({ items }) {
     const [expandedItems, setExpandedItems] = useState(0);
 
+    const handleClick = (nextIndex) => {
+        setExpandedItems(nextIndex);
+    };
+
     const renderItems = items.map((item, index) => {
         const isExpanded = index === expandedItems;
 
         return (
             <div key={item.id}>
-                <div onClick={() => setExpandedItems(index)}>{item.label}</div>
+                <div onClick={() => handleClick(index)}>{item.label}</div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
         );
