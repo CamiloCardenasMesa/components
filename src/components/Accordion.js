fa96402e0f16d23833ea9e7ce69afd8edb7c6de7
Camilo <arqcamilocardenasmesa@gmail.com>
2023-08-02 22:08:58 -0500

    feat: toggling panel collapse

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,14 +2,18 @@ import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 import { useState} from "react";
 
 function Accordion({ items }) {
-    const [expandedItems, setExpandedItems] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
 
     const handleClick = (nextIndex) => {
-        setExpandedItems(nextIndex);
+        if (expandedIndex === nextIndex) {
+            setExpandedIndex(-1);
+        } else {
+            setExpandedIndex(nextIndex);
+        }
     };
 
     const renderItems = items.map((item, index) => {
-        const isExpanded = index === expandedItems;
+        const isExpanded = index === expandedIndex;
 
         const icon = (
         <span className="text-2xl">
