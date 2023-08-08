8d3932f1f0d712f57036a96baef66e67d45db660
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-08-08 11:21:43 -0500

    feat(link): initial implementation

--- src/components/link.js
+++ src/components/link.js
@@ -1,9 +1,15 @@
-function link({ to, children }) {
+import { useContext } from "react";
+import NavigationContext from "../context/navigation";
+
+function Link({ to, children }) {
+   const { navigate } = useContext(NavigationContext);
+
     const handleClick = (event) => {
-        event.preventDefault();    
+        event.preventDefault();   
+        navigate(to); 
     };
 
     return <a onClick={handleClick}>{children}</a>
 }
 
-export default link;
+export default Link;
