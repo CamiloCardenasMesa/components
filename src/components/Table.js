1f25f5ed5aff00beecbc9a1ea79bc7fe08559794
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-08-08 17:55:24 -0500

    feat: building rows

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,27 @@
 function Table({ data }) {
+    const renderedRows = data.map((fruit) => {
+        return (
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td>{fruit.color}</td>
+                <td>{fruit.score}</td>
+            </tr>
+        );
+    });
+
     return (
-        <div>{data.length}</div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Fruit</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderedRows}
+            </tbody>
+        </table>
     );
 }
 
